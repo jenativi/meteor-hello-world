a4c5e32f1536ebb3ddaa67db76b8b7a9bb4d2591
jenativi <jenativi@hawaii.edu>
2024-03-02 20:14:49 -1000

    Hello World HW

---
+++ app/imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor! I am Justin lol!</h1>
+    <h2>Hello world! hehehehe!</h2>
+    <Hello/>
+    <Info/>
+  </div>
+);
